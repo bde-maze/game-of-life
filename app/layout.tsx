b49b34658862a177e1af599ffa84c9ee752c02fe
bde-maze <bde.maze@gmail.com>
2024-04-27 13:29:14 +0200

    [update] - add link to GH

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
+// Packages
 import { ReactNode } from 'react'
-import { Jersey_20_Charted } from 'next/font/google'
+
+// Styles
 import './globals.css'
+
+// Fonts
+import { Jersey_20_Charted } from 'next/font/google'
+
+// Components
+import LinkToGithubRepo from './components/server/LinkToGithubRepo'
 
 const jersey20Charted = Jersey_20_Charted({
   weight: '400',
@@ -12,10 +20,26 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en" className={`${jersey20Charted.variable}`}>
       <body>
-        <h1 className="font-mono text-4xl sm:text-8xl text-center px-5 mb-8">
-          ~ Game of Life ~
-        </h1>
+        <header className="flex justify-between my-8 mx-16">
+          <div className="w-full" />
+          <h1 className="font-mono text-4xl sm:text-8xl text-center px-5 mb-8 w-full whitespace-nowrap">
+            ~ Game of Life ~
+          </h1>
+          <div className="w-full flex justify-end">
+            <LinkToGithubRepo />
+          </div>
+        </header>
+
         {children}
+        <footer className="text-center text-sm my-8 flex w-full justify-center">
+          <a
+            href="https://linktr.ee/barthelemy"
+            target="_blank"
+            className="hover:text-blue-600 transition-colors duration-100"
+          >
+            <span className="ml-2">Made by Barthelemy</span>
+          </a>
+        </footer>
       </body>
     </html>
   )
